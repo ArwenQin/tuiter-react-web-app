ac2d4a7d4da1505fa75eab3fc6f22f0d720411c3
Arwen Qin <arwenqin626@gmail.com>
2023-08-08 23:04:55 +0800

    added remote server

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-const TUITS_API = 'http://localhost:4000/api/tuits';
+
+const TUITS_API = 'https://https://tuiter-node-server-app-694q.onrender.com/api/tuits';
+
 export const createTuit = async (tuit) => { const response = await axios.post(TUITS_API, tuit)
   return response.data;
 }
